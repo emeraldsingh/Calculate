1ad0cd8e85cf24818bad9e223b3a174322d53edf
emeraldsingh <singhe@warhawks.ulm.edu>
2022-02-26 13:56:19 -0500

    Added multiplication

--- Calculate/src/Operations/Operations.java
+++ Calculate/src/Operations/Operations.java
@@ -7,6 +7,12 @@ public class Operations {
 		// TODO Auto-generated method stub
 		System.out.println(add(4,5));
 		System.out.println(sub(4,5));
+		System.out.println(multiply(4,5));
+	}
+
+	private static int multiply(int a, int b) {
+		// TODO Auto-generated method stub
+		return a*b;
 	}
 
 	private static int sub(int a, int b) {
@@ -18,6 +24,7 @@ public class Operations {
 		// TODO Auto-generated method stub
 		return a+b;
 	}
+	
 
 }
 
