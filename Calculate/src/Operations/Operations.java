eaa09aba945fb5f521a94426bf2bc2f5e29204df
emeraldsingh <singhe@warhawks.ulm.edu>
2022-02-26 13:21:06 -0500

    Added addition

---
+++ Calculate/src/Operations/Operations.java
@@ -0,0 +1,17 @@
+package Operations;
+
+
+public class Operations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(add(4,5));
+	}
+
+	private static int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+}
+
