b7e8f01f7d2bfe5fd319e1bd221c2c0ae9983ca5
emeraldsingh <singhe@warhawks.ulm.edu>
2022-02-26 13:46:15 -0500

    Added substraction

--- Calculate/src/Operations/Operations.java
+++ Calculate/src/Operations/Operations.java
@@ -6,6 +6,12 @@ public class Operations {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(add(4,5));
+		System.out.println(sub(4,5));
+	}
+
+	private static int sub(int a, int b) {
+		// TODO Auto-generated method stub
+		return a-b;
 	}
 
 	private static int add(int a, int b) {
